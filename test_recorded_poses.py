1b9289ad5904351a618efa8a8dcc79059d332d86
arnitkun <ankit.nitagartala@gmail.com>
2019-04-05 00:03:57 +0530

    added normalization to coordinates

--- test_recorded_poses.py
+++ test_recorded_poses.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import cv2
+from sklearn import preprocessing
 
 #import posenet
 
@@ -40,7 +41,7 @@ def draw_keypoints_np(img, keypoint_scores, keypoint_coords):
         cv_keypoints.append(cv2.KeyPoint(kc[1], kc[0], 10. * ks))
     out_img = cv2.drawKeypoints(img, cv_keypoints, outImage=np.array([]))
     return out_img
-    
+
 
 def load_pose_data():
     poses_data = {}
@@ -48,8 +49,14 @@ def load_pose_data():
         pose_data = {}
         f_path = os.path.join(record_dir, pose)
         #pose_data['pose_scores'] = np.load(os.path.join(f_path, "pose_scores.npy"))[0:1]
+
         pose_data['keypoint_scores'] = np.load(os.path.join(f_path, "keypoint_scores.npy"))[0, :].tolist()
-        pose_data['keypoint_coords'] = np.load(os.path.join(f_path, "keypoint_coords.npy"))[0, :, :].tolist()
+
+        #pose_data['keypoint_coords'] = np.load(os.path.join(f_path, "keypoint_coords.npy"))[0, :, :].tolist()
+        coords = np.load(os.path.join(f_path, "keypoint_coords.npy"))[0, :].tolist()
+        coords_l2 = preprocessing.normalize(coords, norm='l2').tolist()
+        pose_data['keypoint_coords'] = coords_l2
+
         poses_data[pose] = pose_data
     return poses_data
 
@@ -63,6 +70,9 @@ def do_test():
             image, data[pose]['keypoint_scores'][5:], data[pose]['keypoint_coords'][5:])
         cv2.imshow(pose, image)
         cv2.waitKey(0)
+
+
+
 
 if __name__ == "__main__":
     do_test()
