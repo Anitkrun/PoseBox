fix normalization

test_recorded_poses.py
@@ -35,12 +35,37 @@ DATA FORMAT:
 
 """
 
-def draw_keypoints_np(img, keypoint_scores, keypoint_coords):
-    cv_keypoints = []
-    for ks, kc in zip(keypoint_scores, keypoint_coords):
-        cv_keypoints.append(cv2.KeyPoint(kc[1], kc[0], 10. * ks))
-    out_img = cv2.drawKeypoints(img, cv_keypoints, outImage=np.array([]))
-    return out_img
+def draw_keypoints_np(img, keypoint_scores, keypoint_coords, is_norm=False):
+    if not is_norm:
+        cv_keypoints = []
+        for ks, kc in zip(keypoint_scores, keypoint_coords):
+            cv_keypoints.append(cv2.KeyPoint(kc[1], kc[0], 10. * ks))
+        out_img = cv2.drawKeypoints(img, cv_keypoints, outImage=np.array([]))
+        return out_img
+    else:
+        #TODO: Implement plotting code for normalized coords
+        import matplotlib.pyplot as plt
+
+        for point in keypoint_coords:
+            plt.plot(point[1], -point[0], 'bo')
+        plt.show()
+        return None
+
+def get_norm_coords(keypoint_coords):
+    res = []
+    for point in keypoint_coords:
+        res.append(point[0])
+        res.append(point[1])
+    return custom_norm(res)
+
+def custom_norm(vector):
+    sq = np.square(vector)
+    sm = np.sum(sq)
+    mag = np.sqrt(sm)
+    return np.divide(vector, mag)
+
+def gather(vector):
+    return np.reshape(vector, (-1, 2))
 
 
 def load_pose_data():
@@ -53,7 +78,7 @@ def load_pose_data():
         pose_data['keypoint_scores'] = np.load(os.path.join(f_path, "keypoint_scores.npy"))[0, :].tolist()
         
         keypoint_coords = np.load(os.path.join(f_path, "keypoint_coords.npy"))[0, :].tolist()
-        keypoint_coords_l2 = preprocessing.normalize(keypoint_coords, norm='l2').tolist()
+        keypoint_coords_l2 = gather(get_norm_coords(keypoint_coords)).tolist()
         pose_data['keypoint_coords'] = keypoint_coords
         pose_data['keypoint_coords_l2'] = keypoint_coords_l2
 
@@ -63,13 +88,13 @@ def load_pose_data():
 
 def do_test():
     data = load_pose_data()
-
+    #print(data)
     for pose in data.keys():
         image = np.zeros((480,640,3), np.uint8)
         image = draw_keypoints_np(
-            image, data[pose]['keypoint_scores'][5:], data[pose]['keypoint_coords'][5:])
-        cv2.imshow(pose, image)
-        cv2.waitKey(0)
+            image, data[pose]['keypoint_scores'][5:], data[pose]['keypoint_coords_l2'][5:], is_norm=True)
+        #cv2.imshow(pose, image)
+        #cv2.waitKey(0)
 
 
 